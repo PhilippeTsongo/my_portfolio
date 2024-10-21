8c6a7b9f1924a42c0961ec398632b73b947ffc83
philippetsongo <philippetsongo90@gmail.com>
2024-10-21 12:29:23 +0200

    correct this font family css in app

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,23 @@
 </template>
 
 <style>
-
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playwrite+DE+Grund:wght@100..400&display=swap');
 
 #app {
-// <uniquifier>: Use a unique and descriptive class name
-// <weight>: Use a value from 100 to 900
+  /* Example class using the Montserrat font */
+  .montserrat-regular {
+    font-family: "Montserrat", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400; /* Specify weight between 100 and 900 */
+    font-style: normal; /* Specify normal or italic */
+  }
 
-.montserrat-<uniquifier> {
-  font-family: "Montserrat", sans-serif;
-  font-optical-sizing: auto;
-  font-weight: <weight>;
-  font-style: normal;
+  /* Another example with a bold Montserrat style */
+  .montserrat-bold {
+    font-family: "Montserrat", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 700; /* Bold weight */
+    font-style: normal; 
+  }
 }
-}
-
 </style>
